a8a1e3568cfe828eab2d127274354a7f8b8c62f9
Błażej <blazejknie@gmail.com>
2021-11-07 21:03:04 +0100

    Book Author POJOs implementation getters setters equals toString

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,5 +1,95 @@
 package guru.springframework.spring5webapp.domain;
 
-public class Book {
+import java.util.Objects;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String title;
+	private String isbn;
+	@ManyToMany
+	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+	inverseJoinColumns = @JoinColumn(name = "author_id"))
+	private Set<Author> authors;
+	
+	public Book(Long id, String title, String isbn) {
+		this.id = id;
+		this.title = title;
+		this.isbn = isbn;
+	}
+
+	
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id 
+				+ ", title=" + title 
+				+ ", isbn=" + isbn 
+				+ ", authors=" + authors + "]";
+	}
+
+	
 }
